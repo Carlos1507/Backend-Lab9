a79eec672b9e0ad93a6e9ec80c71ce2c9f2ad50d
Carlos1507 <ayala.carlos@pucp.edu.pe>
2022-11-15 21:13:37 -0500

    inicio

---
+++ src/main/java/com/example/serverlab9/ServerLab9Application.java
@@ -0,0 +1,13 @@
+package com.example.serverlab9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerLab9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerLab9Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/serverlab9/ServerLab9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serverlab9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerLab9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
