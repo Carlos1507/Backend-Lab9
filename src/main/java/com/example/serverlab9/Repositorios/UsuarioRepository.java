a954e94bc54c233c044d88e2eb85c37e779d94e4
Carlos1507 <ayala.carlos@pucp.edu.pe>
2022-11-15 21:39:13 -0500

    login controlador

--- src/main/java/com/example/serverlab9/Repositorios/UsuarioRepository.java
+++ src/main/java/com/example/serverlab9/Repositorios/UsuarioRepository.java
@@ -2,6 +2,10 @@ package com.example.serverlab9.Repositorios;
 
 import com.example.serverlab9.Entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    @Query(value = "select * from usuario where (nombre=?1 and contra =?2)", nativeQuery = true)
+    Usuario validarUsuario(String nombre, String contrasenia);
+
 }
--- src/main/java/com/example/serverlab9/RestControllerImagenes.java
+++ src/main/java/com/example/serverlab9/RestControllerImagenes.java
@@ -1,12 +1,27 @@
 package com.example.serverlab9;
 
 
+import com.example.serverlab9.Entities.Usuario;
+import com.example.serverlab9.Repositorios.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @CrossOrigin
 @RestController
 public class RestControllerImagenes {
+    @Autowired
+    UsuarioRepository usuarioRepository;
 
+    @PostMapping("/usuario/obtener")
+    public ResponseEntity<HashMap<String, Object>> obtenerUsuarioPorNombreContras(@RequestParam("nombre") String nombre,
+                                                                  @RequestParam("contrasenia") String contrasenia){
+        HashMap<String, Object> responseMap = new HashMap<>();
+        Usuario u= usuarioRepository.validarUsuario(nombre, contrasenia);
+        responseMap.put("Usuario", u);
+        return ResponseEntity.status(HttpStatus.OK).body(responseMap);
+    }
 }
